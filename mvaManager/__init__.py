5a0b03aee512409d8e6fe61de068e55a23bc9660
Vivian Chiu <vivian.chiu@live.com>
2019-09-03 22:28:12 -0400

    reset password functionality

--- mvaManager/__init__.py
+++ mvaManager/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__) 
 app.config['SECRET_KEY'] = ' 2345ba6c787654321234a5 c63'
@@ -11,5 +13,11 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.google.mail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
 
 from mvaManager import routes
--- mvaManager/routes.py
+++ mvaManager/routes.py
@@ -2,11 +2,12 @@ import os
 import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
-from mvaManager import app, db, bcrypt
+from mvaManager import app, db, bcrypt, mail
 from mvaManager.forms import (registrationForm, loginForm, updateAccountForm, requestResetForm, resetPasswordForm, 
                               postForm, newPatientForm, newPractitionerForm)
 from mvaManager.models import User, Post, Patient, Practitioner
 from flask_login import login_user, current_user, logout_user, login_required
+from flask_mail import Message
 
 @app.route('/')
 @app.route('/home')
@@ -45,11 +46,41 @@ def login():
     return render_template('login.html', title='Login', form=form)
 
 @app.route('/reset_password', methods=['GET', 'POST'])
-def rest_request():
+def reset_request():
   if current_user.is_authenticated:
     return redirect(url_for('home'))
-    form= requestResetForm()
-    return render_template('resetRequest.html', title='Reset Password', form=form)
+  form = requestResetForm()
+  if form.validate_on_submit():
+    user = User.query.filter_by(email=form.email.data).first()
+    send_reset_email(user)
+    flash('An email has been sent with instructions to reset your password.', 'info')
+    return redirect( url_for('login'))
+  return render_template('requestReset.html', title='Reset Password', form=form)
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_token(token):
+  if current_user.is_authenticated:
+    return redirect( url_for('home'))
+  user = User.verify_reset_token(token)
+  if user is None:
+    flash('This is an invalid or expired token.', 'warning')
+    return redirect( url_for('reset_request'))
+  form = resetPasswordForm()
+  if form.validate_on_submit():
+    hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+    user.password = hashed_password
+    db.session.commit()
+    flash('You password has been updated! You are now able to login.', 'success')
+    return redirect(url_for('login'))
+  return render_template('resetToken.html', title='Reset Password', form=form)
+
+def send_reset_email(user):
+  token = user.get_reset_token()
+  msg = Message('Password Reset Request', sender="mvaManagerSystem@gmail.com", recipients=[user.email])
+  msg.body = f''' To reset your password, visit the following link: 
+{ url_for('reset_token', token=token, _external=True) }
+If you did not make this request then simply ignore this email and no changes will be made.'''
+  mail.send(msg)
 
 @app.route('/logout')
 def logout():
